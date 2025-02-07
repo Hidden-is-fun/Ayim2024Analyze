666c952e015d8b4aefb1d70a4049ab451dff6ef1
Hidden is fun <54774053+Hidden-is-fun@users.noreply.github.com>
2025-02-07 23:17:23 +0900

    Update modified user data

---
+++ ossapiKey.py
@@ -0,0 +1,8 @@
+"""
+Insert your own client ID and client secret here
+If you don't have one then:
+https://osu.ppy.sh/home/account/edit -> OAuth -> New OAuth Application -> Register application
+"""
+client_id = 0
+client_secret5SGAzS9KrFOy = ''
+
